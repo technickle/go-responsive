2a7fcc2264b208ddb7a84380c1cc4e421bc96457
Jeff Herb <herb.jeff@gmail.com>
2013-04-04 11:12:47 -0400

    Splite charts apart

---
+++ js/site/charts-pie.js
@@ -0,0 +1,32 @@
+  $(document).ready(function(){
+
+    chartObj = [{
+        "cId": "#canvasTest-2",
+        "cType": "pie",
+        "cData": [
+          {
+            value: 55,
+            color:"#0088cc"
+          },
+          {
+            value: 22,
+            color: "#fbb450"
+          },
+          {
+            value: 16,
+            color: "#62c462"
+          },
+          {
+            value: 4,
+            color: "#555"
+          },
+          {
+            value: 3,
+            color: "#ee5f5b"
+          }
+        ]}];
+
+      // Call the responsive plugin
+      $.respCharts(chartObj);
+
+    });
