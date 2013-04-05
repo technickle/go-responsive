b1997539a31d1d9f8e265d54045ec0f90192837f
Jeff Herb <herb.jeff@gmail.com>
2013-04-04 23:34:50 -0400

    Added next and previous buttons to all pages

---
+++ js/site/site.js
@@ -0,0 +1,120 @@
+// get the current page url
+var url = window.location.href.toString().split(window.location.host)[1].replace('/go-responsive/','')
+
+// Find the current position in the navigation list
+var current = $('.global-nav').find('a[href="' + url + '"]').addClass('currentPos');
+
+//Get all of the previous links
+var curListItem = current.parent('li');
+
+var prevlink, 
+	previous = $('#prev'),
+	nextlink,
+	next = $('#next');
+
+// Determine if there is a previous and next location
+// Find previous first
+
+//console.log(curListItem.prev().get(0));
+
+// Find Previous Link
+if (curListItem.prev().length === 1 && curListItem.prev().get(0).tagName == "LI") { 
+
+	//console.log('We have a previous sibling list item');
+
+	// Get the previous sibling
+	prevlink = curListItem.prev().children('a');
+
+} else { 
+
+	//console.log('We are at the beginning of the list'); 
+
+	// Move up to the unorder list
+	var parentUL = curListItem.parent();
+
+	// Check to make sure it item is a ul is needed.
+	if (parentUL.parent().length === 1 && parentUL.parent().get(0).tagName == "LI") {
+
+		//console.log("We are nested");
+
+		// New list item
+		var parentListTopic = parentUL.parent();
+
+		// Check to see if there is a previous sibling li
+		if (parentListTopic.prev().length === 1 && parentListTopic.prev().get(0).tagName == "LI") { 
+
+			// Get the last item from the previous top
+			prevlink = parentListTopic.prev().children('ul').children('li').last().children('a');
+
+		} else {
+
+			//console.log('there is no previous menu item.');
+
+			prevlink = false;
+		}
+
+	} else {
+
+		//console.log("We are at the top of the nav. Remove the prev link.");
+
+		prevlink = false;
+	}
+};
+
+if (prevlink) {
+	//console.log("back " + prevlink);
+	prev.attr(nextlink.attr('href'));
+	prev.text(nextlink.text());
+}
+
+// Find Next Linkis another 
+if (curListItem.next().length === 1 && curListItem.next().get(0).tagName == "LI") { 
+
+	// There is a next link
+	//console.log('There is a next child');
+
+	// Get the previous sibling
+	nextlink = curListItem.next().children('a');
+
+} else {
+
+	//console.log('There are not more li in this list');
+
+	// Move up to the unorder list
+	var parentUL = curListItem.parent();
+
+	// Check to make sure it item is a ul is needed.
+	if (parentUL.parent().length === 1 && parentUL.parent().get(0).tagName == "LI") {
+
+		//console.log("We are nested");
+
+		// New list item
+		var parentListTopic = parentUL.parent();
+
+		// Check to see if there is a previous sibling li
+		if (parentListTopic.next().length === 1 && parentListTopic.next().get(0).tagName == "LI") { 
+
+			// Get the last item from the previous top
+			nextlink = parentListTopic.next().children('ul').children('li').first().children('a');
+
+		} else {
+
+			//console.log('there is no previous menu item.');
+
+			nextlink = false;
+		}
+
+	} else {
+
+		//console.log("We are at the top of the nav. Remove the prev link.");
+
+		nextlink = false;
+	}
+
+}
+
+if (nextlink) {
+	//console.log("next " + nextlink);
+	next.attr(nextlink.attr('href'));
+	next.text(nextlink.text());
+}
