ace969528b11d84e192dceb90df2bd9533db329b
Jeff Herb <herb.jeff@gmail.com>
2013-04-05 00:59:54 -0400

    Graph on homepage

---
+++ js/site/home-page-chart.js
@@ -0,0 +1,23 @@
+  $(document).ready(function(){
+
+    chartObj = [{
+        "cId": "#int-trends",
+        "cType": "line",
+        "cData": {
+          labels : ["2007","2008","2009","2010","2011","2012","2013","2014","2015"],
+          datasets : [
+            {
+              strokeColor : "rgba(220,220,220,1)",
+              data : [1200,1275,1325,1370,1400,1480,1510,1700,1720]
+            },
+            {
+              strokeColor : "rgba(151,187,205,1)",
+              data : [400,545,795,1000,1170,1350,1500,1750,2000]
+            }
+          ]
+        }}];
+
+      // Call the responsive plugin
+      $.respCharts(chartObj);
+
+    });
